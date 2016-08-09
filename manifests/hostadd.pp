e33692a118c7a67d632bfde3fd9996fb780f70e5
Tobias Knipping <knipping@tk-schulsoftware.de>
2016-08-09 12:34:30 +0200

    apply usefull changes from other pull-requests

--- manifests/hostadd.pp
+++ manifests/hostadd.pp
@@ -12,8 +12,8 @@ define ipa::hostadd (
   $descinfo = rstrip(join(['Added by HUIT IPA Puppet module on', $timestamp, $desc], ' '))
 
   exec { "hostadd-${host}":
-    command   => "/sbin/runuser -l admin -c \'/usr/bin/ipa host-add ${host} --force --locality=\"${locality}\" --location=\"${location}\" --desc=\"${descinfo}\" --platform=\"${clientpf}\" --os=\"${clientos}\" --password=${otp}\'",
-    unless    => "/sbin/runuser -l admin -c \'/usr/bin/ipa host-show ${host} >/dev/null 2>&1\'",
+    command   => "/sbin/runuser -l admin -c \'kinit -kt /home/admin/admin.keytab admin && /usr/bin/ipa host-add ${host} --force --locality=\"${locality}\" --location=\"${location}\" --desc=\"${descinfo}\" --platform=\"${clientpf}\" --os=\"${clientos}\" --password=${otp}\'",
+    unless    => "/sbin/runuser -l admin -c \'kinit -kt /home/admin/admin.keytab admin && /usr/bin/ipa host-show ${host} >/dev/null 2>&1\'",
     tries     => '60',
     try_sleep => '60',
     require   => Ipa::Serverinstall[$::fqdn],
